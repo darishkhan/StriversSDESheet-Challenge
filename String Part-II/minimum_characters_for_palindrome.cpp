d7312a50a9fbeba629e73446a8da48c4f865fd99
darishkhan <93848997+darishkhan@users.noreply.github.com>
2023-06-14 15:50:12 +0530

    Update minimum_characters_for_palindrome.cpp

--- String Part-II/minimum_characters_for_palindrome.cpp
+++ String Part-II/minimum_characters_for_palindrome.cpp
@@ -1,1 +1,22 @@
 
+bool isPal(string &s, int n)
+{
+	int i=0;
+	while(i<n-i-1)
+	{
+		if(s[i]!=s[n-i-1]) return false;
+		i++;
+	}
+	return true;
+}
+int minCharsforPalindrome(string str) {
+	// Write your code here.
+	int n=str.size();
+	int i=n-1;
+	for(i=n-1;i>=0;i--)
+	{
+		if(isPal(str, i+1)) break;
+	}
+	return n-i-1;
+	
+}
