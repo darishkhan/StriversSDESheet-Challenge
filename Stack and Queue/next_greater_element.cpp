2e13ee8b2e7cd103626870267bdf3a755ee36a06
darishkhan <93848997+darishkhan@users.noreply.github.com>
2023-06-14 21:33:23 +0530

    Create next_greater_element.cpp

---
+++ Stack and Queue/next_greater_element.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+
+vector<int> nextGreater(vector<int> &arr, int n) {
+    // Write your code here
+    stack<int> s;
+    vector<int> ans(n);
+    for(int i=n-1;i>=0;i--)
+    {
+        if(s.empty()) 
+        {
+            ans[i]=-1;
+            s.push(arr[i]);
+        }
+        else if(s.top()>arr[i])
+        {
+            ans[i]=s.top();
+            s.push(arr[i]);
+        }
+        else
+        {
+            while(s.top()<=arr[i])
+            {
+                s.pop();
+                if(s.empty()) break;
+            }
+            if(s.empty()) ans[i]=-1;
+            else ans[i]=s.top();
+            s.push(arr[i]);
+        }
+    }
+    // for(int i:ans) cout<<i<<" ";
+    return ans;
+    // return ans;
+
+}
